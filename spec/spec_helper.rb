fb5dbffaf16ea195becaaaae97dd4ffeb06b9c5f
Brandon Dimcheff <bdimchef-git@wieldim.com>
2011-03-15 14:09:07 -0400

    steal rspec config from mongoid_taggable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
+require 'bundler'
+Bundler.setup
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'mongoid'
+require 'database_cleaner'
 require 'mongoid_touch'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -8,5 +13,19 @@ require 'mongoid_touch'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.mock_with :rspec
+
+  # http://mongoid.org/docs/integration/
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = 'mongoid'
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db('mongoid_touch_test')
 end
